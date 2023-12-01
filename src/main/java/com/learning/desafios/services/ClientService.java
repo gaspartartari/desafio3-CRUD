5fab8078503eb8f3ec959d0d73092a578478e6a2
Gaspar Tartari <gaspartartari@gmail.com>
2023-12-01 20:02:28 -0300

    PUT and DELETE Client

--- src/main/java/com/learning/desafios/services/ClientService.java
+++ src/main/java/com/learning/desafios/services/ClientService.java
@@ -37,6 +37,18 @@ public class ClientService {
         return new ClientDTO(entity);
     }
 
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        Client entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
     private void copyDtoToEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
